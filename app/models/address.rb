8971a3d9ca92ec6ab4b7e58ccad0ccdaac2652cf
Fernando Paredes <nanoid.xd@gmail.com>
2013-05-08 20:59:57 -0700

    Add address and coordinate parser

--- app/models/address.rb
+++ app/models/address.rb
@@ -14,6 +14,45 @@ class Address < ActiveRecord::Base
   end
 
   def api_query
-    "#{api_url}&amp;address=#{minimal_clean_address}
+    "#{api_url}&amp;address=#{minimal_clean_address}"
   end
+
+  def geocode
+    open(api_query) do |file|
+      @body = file.read
+      doc = Hpricot(@body)
+      parse_response(doc)
+    end
+  end
+
+  def parse_response(doc)
+    self.error_code = (doc/:status).first.inner_html
+    if error_code.eql? "OK"
+      set_coordinates(doc)
+      complete_address(doc)
+    end
+  end
+
+  def set_coodinates(doc)
+    self.latitude = (doc/:geometry/:location/:lat).first.inner_html
+    self.longitude = (doc/:geometry/:location/:lng).first.inner_html
+  end
+
+  def complete_address(doc)
+    (doc/:result/:address_component).each do |ac|
+      if (ac/:type).first.inner_html == "sublocality"
+        self.suburb = (ac/:long_name).first.inner_html
+      end
+
+      if (ac/:type).first.inner_html == "administrative_area_level_3"
+        self.county = (ac/:long_name).first.inner_html
+      end
+
+      if (ac/:type).first.inner_html == "administrative_area_level_1"
+        self.state = (ac/:long_name).first.inner_html
+      end
+    end
+  end
+
+
 end
