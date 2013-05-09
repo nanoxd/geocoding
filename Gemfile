6f14922dd485df0ce744c47db557a9f35eca283a
Fernando Paredes <nanoid.xd@gmail.com>
2013-05-08 20:50:51 -0700

    Add address parser and accompanying api calls.

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 gem 'rails', '4.0.0.beta1'
 
 gem 'sqlite3'
-
+gem 'hpricot'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,19 @@
 class Address < ActiveRecord::Base
+  require 'open-uri'
+  require 'cgi'
+  require 'hpricot'
+
+  attr_accessible :city, :country, :error_code, :latitude, :longitude, :state, :street, :streetno, :suburb, :zipcode
+
+  def minimal_clean_address
+    [streetno, street, city, zip_code, country].to_a.compact.join(",")
+  end
+
+  def api_url
+    "http://maps.googleapis.com/maps/api/geocode/xml?sensor=false"
+  end
+
+  def api_query
+    "#{api_url}&amp;address=#{minimal_clean_address}
+  end
 end
